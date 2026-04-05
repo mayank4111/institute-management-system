585a30362d622bbaab044db1a3f3bdc81dc4ed2f
Mayank Kumar <mayankkumar4111@gmail.com>
2026-04-06 00:41:49 +0530

    Update models and views

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Course, ContactMessage, AboutUs, FAQ, Testimonial
+
+admin.site.register(Course)
+admin.site.register(ContactMessage)
+admin.site.register(AboutUs)
+admin.site.register(FAQ)
+admin.site.register(Testimonial)
